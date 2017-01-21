52541cd2f6bd3ac94b228232ab459c3c61629291
njgolen <njgolen@gmail.com>
2017-01-21 02:29:32 -0600

    Changed the non-animated sprites to have their own files, seperated the artassets.js file from utility.js.

---
+++ code/ss/artassets.js
@@ -0,0 +1,32 @@
+var playerSprites = document.createElement("img");
+playerSprites.src = "img/player.png";
+
+var lavaSprite = document.createElement("img");
+lavaSprite.src = "img/lavaSprite.png";
+
+var brickSprite = document.createElement("img");
+brickSprite.src = "img/brickSprite.png";
+
+var coinSprite = document.createElement("img");
+coinSprite.src = "img/coinSprite.png";
+
+//var otherSprites = document.createElement("img");
+//otherSprites.src = "img/sprites.png";
+
+//var brickSprite = document.createElement("img");
+//brickSprite.src = "img/sprites1.png";
+
+//var lavaSprite = document.createElement("img");
+//lavaSprite.src = "img/sprites2.png";
+
+//var coinSprite = document.createElement("img");
+//coinSprite.src = "img/sprites3.png";
+
+//function loadSpriteFrames(spriteArray, fileBaseName, spriteCount) {
+//    //spriteArray = [];
+//    for (i = 0; i < spriteCount; i++) {
+//        spriteArray[i] = document.createElement("img");
+//        spriteArray[i].src = "img/" + fileBaseName + (i + 1) + ".png";
+//    }
+//    //return spriteArray;
+//}
