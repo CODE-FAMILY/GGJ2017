8c9313e6b6e7128ac463cff3d5e877aff0da189b
Jeeppler <jeremias.eppler@mailbox.org>
2017-01-21 18:21:35 -0500

    intro link fix

--- code/ss/menu.js
+++ code/ss/menu.js
@@ -6,8 +6,15 @@ window.onload = function() {
   svgDoc = document.getElementById("menu").contentDocument;
 
   playbtn = svgDoc.getElementById("PLAYBUTTON");
-  playbtn.addEventListener("click", function() { window.location="/index.html" });
+
+  index = svgDoc.createElement("a");
+  index.setAttribute("href", "index.html");
+
+  playbtn.appendChild(index);
+
+  console.log(window.location.href);
+  playbtn.addEventListener("click", function() { window.location.href="index.html" });
 
   creditsbtn = svgDoc.getElementById("CREDITSBUTTON");
-  creditsbtn.addEventListener("click", function() { window.location="/credits.html" });
+  creditsbtn.addEventListener("click", function() { window.location.href="credits.html" });
 }
