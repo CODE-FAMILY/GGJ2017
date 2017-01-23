cb641e9f1f975254ccc1a341157f0e9a028bef60
Jeeppler <jeremias.eppler@mailbox.org>
2017-01-23 15:31:22 -0500

    menu link fix

--- code/ss/menu.js
+++ code/ss/menu.js
@@ -6,8 +6,16 @@ window.onload = function() {
   svgDoc = document.getElementById("menu").contentDocument;
 
   playbtn = svgDoc.getElementById("PLAYBUTTON");
-  playbtn.addEventListener("click", function() { window.location.href="../index.html" });
+  playbtn.addEventListener("click", function() {
+    var current = window.location.href
+    var newLink = current.replace("intro.html", "index.html");
+    window.location.href=newLink;
+  });
 
   creditsbtn = svgDoc.getElementById("CREDITSBUTTON");
-  creditsbtn.addEventListener("click", function() { window.location.href="../credits.html" });
+  creditsbtn.addEventListener("click", function() {
+    var current = window.location.href
+    var newLink = current.replace("intro.html", "credits.html");
+    window.location.href=newLink;
+  });
 }
